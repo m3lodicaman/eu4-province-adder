e7f41d5074a8fe8a7a71733706d33c12b5a2cf89
m3lo <33330721+m3lodicaman@users.noreply.github.com>
2018-03-17 12:58:11 -0400

    Finilized structure of filesystem editor

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 outputFile = "C:/Users/Kai/Documents/Output/writeTo.txt";
 
+##with open(outputFile, 'r+') as f:
+##   f.write('This is a test\n');
+
+with open(outputFile, 'r') as f:
+    x = f.readlines();
+
+y = len(x[1]);
+x[1] = x[1][0:(y-1)] + ' dick\n';
+
 with open(outputFile, 'r+') as f:
-    f.write('This is a test\n');
+    f.writelines(x);
